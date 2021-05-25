4958d1fa29f069095574a6b7cd7dea8a2202108d
asbyth <cyronize@gmail.com>
2021-05-25 07:33:34 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/mods/autocomplete/tweaker/AutocompleteTweaker.java
+++ src/main/java/club/sk1er/mods/autocomplete/tweaker/AutocompleteTweaker.java
@@ -1,9 +1,9 @@
 package club.sk1er.mods.autocomplete.tweaker;
 
-import gg.essential.loader.EssentialTweaker;
+import gg.essential.loader.EssentialSetupTweaker;
 
 @SuppressWarnings("unused")
-public class AutocompleteTweaker extends EssentialTweaker {
+public class AutocompleteTweaker extends EssentialSetupTweaker {
     public AutocompleteTweaker() {
         super(new String[]{});
     }
